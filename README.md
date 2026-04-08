97e78ef4d1fa3cb855d83580cf9f53916e79d945
maida5 <145588313+maida5@users.noreply.github.com>
2026-04-08 16:27:02 -0400

    feat: emailing functionality

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# csci39531-practicum
+# # csci39531-practicum
+
+# Key Features
+
+-  [x] Emailing
+
+# Emailing
+
+Basic Logic:
+
+-  we use the smtp and email libraries provided by Python! thanks python >\_<
+-  takes in: subject, content, link, LIST of photo attachments
+-  set subject, recipient, content...
+-  add LINK IF PROVIDED as an "attachment" \*\* This could have a better method
+-  go through list of images and use requests library to get the BINARY DATA of the image, and attach it as an attachment
+-  if any image processing fails... we note that in the email
+-  once all of that is DONE...
+-  open an SMTP server to GMAIL on port 465 \*\* i got this from lecture but some sources say another port is more secure, however the other port was not working with the method i was going for for some reason so i stuck with this
+-  send the message!
+-  print if fail...
+
+IMPORTANT Features TBD:
+
+-  Logging!
+-  go through the double starred items idk
+
+Fun... Features:
+
+-  perhaps make the email format cuter haha. just some fun
---
+++ send_email.py
@@ -0,0 +1,64 @@
+import smtplib
+from email.message import EmailMessage
+
+# get variables from .env
+import os
+from dotenv import load_dotenv
+
+import requests
+
+# load env var
+load_dotenv()
+
+def send_email(subject, content, link, photo_attachments):
+   msg = EmailMessage()
+
+   msg["Subject"] = subject
+   msg["To"] = os.getenv("RECIPIENT_EMAIL")
+
+   msg.set_content(content)
+
+   if link:
+      msg.add_attachment(f"link to the product: {link}")
+
+   # ### NEED TO ADD BETTER ERROR HANDLING HERE
+   # want to keep track of failed images sent!
+   failed_images = 0
+
+   # going through list of attachments...
+   for photo in photo_attachments:
+      try:
+         # as a with statement to not worry about releasing resources
+         with requests.get(photo, stream=True) as response:
+            response.raise_for_status()
+
+            # this is to get the type of img it is (jpg, etc)
+            types = response.headers['Content-Type'].split('/')
+            maintype, subtype = types[0], types[1]
+
+            # binary data of the image
+            img_data = response.content
+            msg.add_attachment(img_data, maintype=maintype, subtype=subtype)
+      except Exception as e:
+         failed_images+=1
+         print(f"Error attaching image: {e}")
+
+   # let me know if there are any fail images in the email itself! wow
+   if failed_images:
+      msg.add_attachment(f"{failed_images} image/s failed to be attached T_T")
+
+   try:
+      # i got this from the lecture! but many sources online use another port. we can investigate. later...
+      with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+         smtp.login(os.getenv("sender_email"), os.getenv("pass"))
+         smtp.send_message(msg)
+         print("msg sent!")
+   except Exception as e:
+      print(f"message failed...: {e}")
+   
+   
+# # TESTING
+# link = "https://t.co/Kt7VzDRdKq"
+# imgs = ["https://pbs.twimg.com/media/HFVNWlEa8AAl0c9?format=jpg&name=medium", "https://pbs.twimg.com/media/HFRJz1mbEAARfuB?format=jpg&name=medium"]
+
+# send_email("test","test",link,imgs)
